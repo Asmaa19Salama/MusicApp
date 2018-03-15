3f03dd88c4afa32f33eceb9dc2195bf2f04ee77f
asmaa salama <asmaamilad@gmail.com>
2018-03-15 21:27:35 +0200

    Changes in the functionality of some classes.

--- app/src/main/java/com/example/android/musicapp/Badanamu.java
+++ app/src/main/java/com/example/android/musicapp/Badanamu.java
@@ -1,34 +1,49 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class Badanamu extends AppCompatActivity {
+public class Badanamu extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.song_list);
 
-        // Create a list of words
         ArrayList<Song> songs = new ArrayList<Song>();
         songs.add(new Song("Fun Run", "Badanamu"));
         songs.add(new Song("Hoola Hoola Hooping", "Badanamu"));
         songs.add(new Song("Sing the Alphabet", "Badanamu"));
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
         SongAdapter adapters = new SongAdapter(this, songs);
 
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml.xmlt file.
-        ListView listView = (ListView) findViewById(R.id.list);
+        //ListView listView = (ListView) findViewById(R.id.list_and_button);
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
+        //listView.setAdapter(adapters);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.list_and_button);
+        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapters);
+
+        Button badanamu = (Button) findViewById(R.id.change_class);
+
+        onClick(badanamu);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vIntent = new Intent(new Badanamu(), Play.class);
+                startActivity(vIntent);
+            }
+        });
     }
 }
